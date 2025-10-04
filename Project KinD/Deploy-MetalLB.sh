b2593d33f816b1bb4af7607530528affa07c5fee
n70arun <59335370+n70arun@users.noreply.github.com>
2025-10-04 12:25:12 +0100

    Update Deploy-MetalLB.sh

--- Project KinD/Deploy-MetalLB.sh
+++ Project KinD/Deploy-MetalLB.sh
@@ -1,2 +1,2 @@
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.9/config/manifests/metallb-native.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.9/config/manifests/MetalLB-Config.yaml
 kubectl apply -f metallb-config.yaml
