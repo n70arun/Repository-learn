52f936afe32d7d82f4025fe009407be97f468a26
arun subramanian <arun@aruns-MacBook-Air.local>
2025-10-04 16:03:54 +0100

    adding Kind docker network

--- Project KinD/Deploy-KIND-Cluster.sh
+++ Project KinD/Deploy-KIND-Cluster.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 kind delete cluster --name kind-cluster-pandora
-kind create cluster --name kind-cluster-pandora --config K8S-Config-1CP-2W.yaml
+docker network rm kind kind-net || true
+docker network create --driver=bridge --subnet=172.20.0.0/16 kind-net
+#kind create cluster --name kind-cluster-pandora --config K8S-Config-1CP-2W.yaml
+KIND_EXPERIMENTAL_DOCKER_NETWORK=kind-net kind create cluster --config K8S-Config-1CP-2W.yaml
 
 
